bbd562b5f73e2ffa7718878f5af931cd6bd3bd85
Vladimir Bulyga <zero@13w.me>
2019-10-22 09:54:03 +0300

    continue working with multiple databases
    Signed-off-by: Vladimir Bulyga <zero@13w.me>

---
+++ libs/db/drivers/sqlite.js
@@ -0,0 +1,28 @@
+const SQLDriver = require('./driver');
+
+const mysql = require('sqlite3');
+
+class SQLiteDriver extends SQLDriver {
+  async connect() {
+    this.pool = await mysql.createPool(this.config);
+  }
+
+  async disconnect() {
+    await this.pool.end();
+  }
+
+  async getConnection() {
+    const conn = await this.pool.getConnection();
+    conn.config.namedPlaceholders = true;
+    return conn;
+  }
+
+  async query(sql, params) {
+    const conn = await this.getConnection();
+    const result = await conn.query(sql, params);
+    await conn.release();
+    return result;
+  }
+}
+
+module.exports = SQLiteDriver;
